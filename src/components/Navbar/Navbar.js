174d0eb22bc761e203c4c88cd770ab071b6afc31
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-07 02:18:43 +0530

    done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
+import "./Navbar.css"
 function Navbar() {
     return (
-        <Navbar>
-            Akhilesh Sharma
-        </Navbar>
+        <nav className="navbar" >
+            <div className="logo" >Logo</div>
+            <div className="searchbox" >SearchBOX</div>
+            <div className="login" >Login</div>
+            <div className="cart" >Cart</div>
+        </nav>
     )
 }
 
