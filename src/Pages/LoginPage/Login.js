1dcace2b9c2f61bef5a3ed1e059dd45a2803bf5b
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-21 15:01:01 +0530

    done

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import style from "./Login.module.css";
 
 function LoginPage() {
-    return (<div>
-        <input />
-        <input />
-        <button>Login</button>
+    const [login, setLogin] = useState(true)
+
+    function loginHandler(e) {
+        e.preventDefault()
+        setLogin(false)
+    }
+    function signupHandler(e) {
+        e.preventDefault()
+        setLogin(true)
+    }
+
+    return (<div className={style.FormContainer} >
+        {
+            login === true ?
+                <form className={style.loginFormContainer} >
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Passworrd" />
+                    <button>Login</button>
+                    <button onClick={loginHandler} >New User? Signup</button>
+                </form> :
+                <form className={style.signUpFormContainer} >
+                    <input type="email" placeholder="Email Address" />
+                    <input type="text" placeholder="Full Name" />
+                    <input type="password" placeholder="Set Password" />
+                    <input type="password" placeholder="Repeat Password" />
+                    <input type="number" placeholder="Mobile Number" />
+                    <input type="text" placeholder="Full Address" />
+                    <input type="number" placeholder="Pincode" />
+                    <button>Signup</button>
+                    <button onClick={signupHandler} >Back to Login</button>
+                </form>
+        }
     </div>)
 }
 export default LoginPage;
