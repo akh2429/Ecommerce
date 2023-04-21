7d146af5de780cf724b86f9c66349b34ec305f47
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-21 15:49:07 +0530

    done

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -17,21 +17,21 @@ function LoginPage() {
         {
             login === true ?
                 <form className={style.loginFormContainer} >
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Passworrd" />
-                    <button>Login</button>
-                    <button onClick={loginHandler} >New User? Signup</button>
+                    <input className={style.inputContainer} type="email" placeholder="Email Address" />
+                    <input className={style.inputContainer} type="password" placeholder="Password" />
+                    <button className={style.button} >Login</button>
+                    <button className={style.button} onClick={loginHandler} >New User? Signup</button>
                 </form> :
                 <form className={style.signUpFormContainer} >
-                    <input type="email" placeholder="Email Address" />
-                    <input type="text" placeholder="Full Name" />
-                    <input type="password" placeholder="Set Password" />
-                    <input type="password" placeholder="Repeat Password" />
-                    <input type="number" placeholder="Mobile Number" />
-                    <input type="text" placeholder="Full Address" />
-                    <input type="number" placeholder="Pincode" />
-                    <button>Signup</button>
-                    <button onClick={signupHandler} >Back to Login</button>
+                    <input className={style.inputContainer} type="email" placeholder="Email Address" />
+                    <input className={style.inputContainer} type="text" placeholder="Full Name" />
+                    <input className={style.inputContainer} type="password" placeholder="Set Password" />
+                    <input className={style.inputContainer} type="password" placeholder="Repeat Password" />
+                    <input className={style.inputContainer} type="number" placeholder="Mobile Number" />
+                    <input className={style.inputContainer} type="text" placeholder="Full Address" />
+                    <input className={style.inputContainer} type="number" placeholder="Pincode" />
+                    <button className={style.button} >Signup</button>
+                    <button className={style.button} onClick={signupHandler} >Back to Login</button>
                 </form>
         }
     </div>)
