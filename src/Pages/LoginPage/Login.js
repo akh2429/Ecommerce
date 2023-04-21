7ea77b5361f721547445a71434fb7b3fbf489d8d
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-22 01:15:28 +0530

    done

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -16,7 +16,10 @@ function LoginPage() {
     }
     function UserSignup(e) {
         e.preventDefault();
-        console.log(signUpuser);
+        const existingUsers = JSON.parse(localStorage.getItem('userDetails')) || [];
+        const updatedUsers = [...existingUsers, signUpuser];
+        localStorage.setItem('userDetails', JSON.stringify(updatedUsers));
+        setsignUpuser({ id: Math.random(), email: '', fullname: '', newPassword: '', repeatPassword: '', mobileNumber: '', fullAddress: '', pincode: '' })
     }
     function UserLogin(e) {
         e.preventDefault();
@@ -27,19 +30,19 @@ function LoginPage() {
         {
             login === true ?
                 <form className={style.loginFormContainer} >
-                    <input className={style.inputContainer} type="email" placeholder="Email Address" onChange={loginHandler} name="email" value={loggedUser.email} />
-                    <input className={style.inputContainer} type="password" placeholder="Password" onChange={loginHandler} name="password" value={loggedUser.password} />
+                    <input type="email" placeholder="Email Address" onChange={loginHandler} name="email" value={loggedUser.email} />
+                    <input type="password" placeholder="Password" onChange={loginHandler} name="password" value={loggedUser.password} />
                     <button className={style.button} onClick={UserLogin} >Login</button>
                     <button className={style.button} onClick={() => setLogin(false)} >New User? Signup</button>
                 </form> :
                 <form className={style.signUpFormContainer} >
-                    <input className={style.inputContainer} type="email" placeholder="Email Address" onChange={signUpHandler} value={signUpuser.email} name="email" />
-                    <input className={style.inputContainer} type="text" placeholder="Full Name" onChange={signUpHandler} value={signUpuser.fullname} name="fullname" />
-                    <input className={style.inputContainer} type="password" placeholder="Set New Password" onChange={signUpHandler} value={signUpuser.newPassword} name="newPassword" />
-                    <input className={style.inputContainer} type="password" placeholder="Repeat Password" onChange={signUpHandler} value={signUpuser.repeatPassword} name="repeatPassword" />
-                    <input className={style.inputContainer} type="number" placeholder="Mobile Number" onChange={signUpHandler} value={signUpuser.mobileNumber} name="mobileNumber" />
-                    <input className={style.inputContainer} type="text" placeholder="Full Address" onChange={signUpHandler} value={signUpuser.fullAddress} name="fullAddress" />
-                    <input className={style.inputContainer} type="number" placeholder="Pincode" onChange={signUpHandler} value={signUpuser.pincode} name="pincode" />
+                    <input type="email" placeholder="Email Address" onChange={signUpHandler} value={signUpuser.email} name="email" />
+                    <input type="text" placeholder="Full Name" onChange={signUpHandler} value={signUpuser.fullname} name="fullname" />
+                    <input type="password" placeholder="Set New Password" onChange={signUpHandler} value={signUpuser.newPassword} name="newPassword" />
+                    <input type="password" placeholder="Repeat Password" onChange={signUpHandler} value={signUpuser.repeatPassword} name="repeatPassword" />
+                    <input type="number" placeholder="Mobile Number" onChange={signUpHandler} value={signUpuser.mobileNumber} name="mobileNumber" />
+                    <input type="text" placeholder="Full Address" onChange={signUpHandler} value={signUpuser.fullAddress} name="fullAddress" />
+                    <input type="number" placeholder="Pincode" onChange={signUpHandler} value={signUpuser.pincode} name="pincode" />
                     <button className={style.button} onClick={UserSignup} >Signup</button>
                     <button className={style.button} onClick={() => setLogin(true)} > Back to Login</button>
                 </form>
