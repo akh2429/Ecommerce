31b5d283124b3c900fd54cd33299921c56199daf
Akhilesh Sharma <akhileshsharma2429@gmail.com>
2023-05-17 09:48:50 +0530

    done

--- E Commerce Front End/src/Pages/Productslanding/productcomponent/productcomponent.js
+++ E Commerce Front End/src/Pages/Productslanding/productcomponent/productcomponent.js
@@ -1,23 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function Products({ data }) {
     console.log(data);
     return (
         <div className="flex gap-3 shadow-md flex-wrap items-start justify-start  ">
             {data &&
                 data.map((val) => (
-                    <div
-                        className=" 
-                    flex 
-                    flex-col 
-                     w-72 rounded-b-3xl 
-                     p-1 items-center 
-                     overflow-hidden 
-                     flex-nowrap 
-                     border-4
-                      border-gray-950 
-                      border-solid 
-                      gap-2"
-                    >
+
+                    <div className=" flex flex-col w-72 rounded-b-3xl p-1 items-center overflow-hidden flex-nowrap border-4 border-gray-950 border-solid gap-2" >
                         <img
                             className=" top-0 left-0 h-full w-full object-cover z-0"
                             src={val.images}
@@ -29,24 +19,16 @@ function Products({ data }) {
                             </div>
                             <div className="flex p-1">{val.price}$/-</div>
                         </div>
-                        <button
-                            className="
-                        flex p-1 
-                        items-center 
-                        justify-center 
-                        rounded-ss-xl z-10
-                         bg-black
-                          bg-white 
-                          border-2
-                           border-gray-950 
-                           font-extrabold 
-                           hover:text-lg  "
-                        >
+                        <Link to={`/finalCheckout?id=${val._id}`}
+                            className="flex p-1 items-center justify-center rounded-ss-xl z-10 bg-black bg-white  border-2  border-gray-950   font-extrabold   hover:text-lg  ">
                             More Details
-                        </button>
+                        </Link>
                     </div>
-                ))}
-        </div>
+
+
+                ))
+            }
+        </div >
     );
 }
 
