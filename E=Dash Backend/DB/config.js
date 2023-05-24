72fc8e653079506887db71080df291c075c043fe
Akhilesh Sharma <akhileshsharma2429@gmail.com>
2023-05-24 21:27:28 +0530

    done

--- E=Dash Backend/DB/config.js
+++ E=Dash Backend/DB/config.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const db = "mongodb+srv://admin:admin@cluster0.9afgoex.mongodb.net/E-com?retryWrites=true&w=majority";
-
+const db = process.env.MONGO_CONNECTION
 mongoose.connect(db).then(() => {
     console.log("Connection Sucessful")
-}).catch(() => console.log("Error in connecting Database"))
+}).catch(() => console.log(db));
--- E=Dash Backend/index.js
+++ E=Dash Backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("./DB/config.js");
 const express = require("express");
 const app = express();
