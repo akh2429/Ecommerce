6d8faa4ab87d9757aefcf8a833df03b05ffdfcfc
Akhilesh Sharma <akhileshsharma2429@gmail.com>
2023-05-24 21:47:41 +0530

    done

--- E=Dash Backend/DB/config.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const db = process.env.MONGO_CONNECTION
-mongoose.connect(db).then(() => {
-    console.log("Connection Sucessful")
-}).catch(() => console.log(db));
